c33ac20e11e0afa557e2c041ecfcb1857426a027
Flavio Kon <flaviokr@gmail.com>
2015-07-24 15:47:49 +0000

    Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   
-  get 'listas/vila_mariana'
-  get 'listas/vila_guilherme'
-  get 'listas/licenca_medica'
-  get 'listas/falta'
-  get 'listas/recorps'
-  get 'listas/outras_areas'
-  get 'listas/readaptados'
-  get 'listas/aposentados'
-  get 'listas/geral'
+  get 'home'           => 'pages#home'
+  get 'vila_mariana'   => 'listas#vila_mariana'
+  get 'vila_guilherme' => 'listas#vila_guilherme'
+  get 'licenca_medica' => 'listas#licenca_medica'
+  get 'falta'          => 'listas#falta'
+  get 'recorps'        => 'listas#recorps'
+  get 'outras_areas'   => 'listas#outras_areas'
+  get 'readaptados'    => 'listas#readaptados'
+  get 'aposentados'    => 'listas#aposentados'
+  get 'geral'          => 'listas#geral'
   resources :motoristas
   
 end
