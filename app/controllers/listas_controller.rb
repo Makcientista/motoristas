f7f42d28660a1266d0407568dc2989f1bf6c8eb6
Flavio Kon <flaviokr@gmail.com>
2015-07-24 16:19:26 +0000

    Listas controller

--- app/controllers/listas_controller.rb
+++ app/controllers/listas_controller.rb
@@ -1,29 +1,38 @@
 class ListasController < ApplicationController
   
   def vila_mariana
+    @motoristas = Motorista.where("status = 'Vila Mariana'")
   end
 
   def vila_guilherme
+    @motoristas = Motorista.where("status = 'Vila Guilherme'")
   end
 
   def licenca_medica
+    @motoristas = Motorista.where("status = 'Licença Médica'")
   end
 
   def falta
+    @motoristas = Motorista.where("status = 'Processo Administrativo de Falta'")
   end
 
   def recorps
+    @motoristas = Motorista.where("status = 'Recorps'")
   end
 
   def outras_areas
+    @motoristas = Motorista.where("status = 'Outras Áreas'")
   end
 
   def readaptados
+    @motoristas = Motorista.where("status = 'Readaptado'")
   end
 
   def aposentados
+    @motoristas = Motorista.where("status = 'Aposentado'")
   end
   
   def geral
+    @motoristas = Motorista.all
   end
 end
