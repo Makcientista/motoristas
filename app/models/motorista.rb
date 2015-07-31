be59e9ea897089f2e1f568937f52a807996a4315
Flavio Kon <flaviokr@gmail.com>
2015-07-31 13:38:57 +0000

    Update Motorista Model

--- app/models/motorista.rb
+++ app/models/motorista.rb
@@ -2,12 +2,12 @@ class Motorista < ActiveRecord::Base
   
  validates :nome, presence: true
  validates :rf, presence: true
- validates :cargo, presence: true
- validates :secao_original, presence: true
- validates :secao_atual, presence: true
- validates :pontuacao, presence: true
- validates :local, presence: true
- validates :status, presence: true
+ # validates :cargo, presence: true
+ # validates :secao_original, presence: true
+ # validates :secao_atual, presence: true
+ # validates :pontuacao, presence: true
+ # validates :local, presence: true
+ # validates :status, presence: true
  
   # locais possiveis: 
   #    'TRAF V. MARIAN.'
